ef3390accac9cee5c7d6d12b8e61fa4952dac2d3
Alice Developer <alice@company.com>
2018-12-14 23:09:32 +0800

    just motify

--- src/rand.c
+++ src/rand.c
@@ -2,29 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-void init_rand(void)
+void
+init_rand (void)
 {
- srand(time(NULL))
-}
+srand (time (NULL))}
 
 //TODO: use a better random generator
-a8b8878633cb262503760f8f8c46cf3147e016cb
-int random_int(int max)
+int
+random_int (int max)
 {
-  return rand() % max;
+  return rand () % max;
 }
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-  if(argc != 2){
-   fprintf(stderr,"Usage: %s <number>\n", argv[0]);
-   return EXIT_FAILURE;
- }
-  int max = atoi(argv[1]);
+  if (argc != 2)
+    {
+      fprintf (stderr, "Usage: %s <number>\n", argv[0]);
+      return EXIT_FAILURE;
+    }
+  int max = atoi (argv[1]);
 
-  srand(time(NULL))
-  int result = random_int(max);
-  printf("%d\n",result);
+  srand (time (NULL)) int result = random_int (max);
+  printf ("%d\n", result);
 
   return EXIT_SUCCESS;
 }
